54120501a2a68f55425ccaec5ae505737b004d87
Matthias Blom <matthias.blom@hotmail.com>
2020-06-13 15:30:44 +0000

    Updated help

--- commands/help.js
+++ commands/help.js
@@ -21,13 +21,12 @@ const helpMessage = `
 Please use the date when naming an event (e.g. Thursday Night Ops 14/5).
 The time of the event should be in YYYY-MM-DD hh:mm format (e.g. 2020-05-17 17:00).
 List of current commands:
-    ($help)              Displays this help message.
-    ($addChannel)        Adds the channel to the whitelist.
-    ($removeChannel)     Removes the channel from the whitelist.
-    ($role add|remove)   Adds or removes the minimum role required to use the bot on a server
+    ($help)               Displays this help message.
+    ($channel add|remove) Adds or removes the channel to/from the whitelist.
+    ($role add|remove)    Adds or removes the minimum role required to use the bot on a server
     Events:
-    ($event delete)      Delete event.
-    ($event PS2OP)       Sets up a PS2 Op.'
-    ($event Training)    Sets up a PS2 Training.
+    ($event delete)       Delete event.
+    ($event PS2OP)        Sets up a PS2 Op.'
+    ($event Training)     Sets up a PS2 Training.
 \`\`\`
 `
