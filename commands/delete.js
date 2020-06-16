1f1c0a26617f958a2a910a12de596bd8c3d3353d
Combustiblemon <tasos.hitiris@hotmail.com>
2020-06-17 00:22:56 +0300

    added comments

--- commands/delete.js
+++ commands/delete.js
@@ -30,21 +30,21 @@ module.exports = {
         //find only the messages in the current channel
         tempArray = await messagesInChannel(originalChannel, tempArray);
         
+        //Replace the '_' in the name with ' '
         tempArray.forEach(function(item, index) {
             tempArray[index] = item.replace(/_/gi, " ");
         });
         
         let question = '```Which of the following events would you like to delete?\n     ' + tempArray.join("\n     ") + '```';
 
-        
-
-        //let answer = await requestDetail(question, message);
         let answer = await EventDetailsService.prototype.requestSingleDetail(question, message);
         if (answer === ''){
             return;
         }
+        
+        //replace the ' ' in the answer with '_' so it matches the file naming structure 
         answer = answer.replace(/ /gi, '_');
-        console.log(`answer: ${answer}`);
+        //check if the the name exists
         if(FileSystem.embedNameExists(answer)){
             deleteEmbed(answer.replace(/ /gi, '_'), originalChannel);
             message.author.send(`Event \`${answer}\` deleted.`);
@@ -60,18 +60,21 @@ module.exports = {
  * @param {TextChannel} channel 
  */
 function deleteEmbed(answer, channel){
+    //convert the embed name into ID
     let msgID = FileSystem.getEmbedID(answer);
     if (msgID === null){
         console.error(new nullEmbedID('Embed does not exist in memory'));
         return;
     }
 
-    console.log(msgID);
+    //delete the message
     channel.messages.fetch(msgID).then(msg =>{ msg.delete()}).catch(error => {console.log(error)});
     
+    //remove references to embed
     FileSystem.removeEmbedID(FileSystem.getEmbedID(answer));
     FileSystem.removeEmbedName(answer.replace(/_/gi, ' '));
 
+    //remove the files
     fs.unlink('./embeds/' + answer + '.json', (err) => {
         if (err) throw err;
         console.log(`embeds/${answer}.json was deleted.`);
