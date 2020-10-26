ab750d304bcc4db24128498e9c34a91aed154f5e
Combustiblemon <tasos.hitiris@hotmail.com>
2020-10-26 18:23:46 +0200

    changed help message

--- commands/help.js
+++ commands/help.js
@@ -9,9 +9,31 @@ module.exports = {
      */
     async execute(bot, message) {
 
-        message.author.send(helpMessage);
+        message.author.send(this.createEmbed());
+    },
+
+    createEmbed(){
+        let embed = new Discord.MessageEmbed()
+            .setTitle('Help Message.')
+            .setDescription(description)
+            .setColor(0xF1C40F);
+
+            embed.addField('List of commands:', commands)
+        
+        return embed;
     }
 }
+
+const description = `For any extra help message CombustibleLemon#6917.
+                     [Report bugs/request features](https://github.com/Combustiblemon/Discord-Event-Bot/issues).
+                     The time of the event should be in YYYY-MM-DD hh:mm format (e.g. 2020-05-17 17:00).
+                     After the event is over use command '$event delete' to delete your event.`
+
+const commands =`**($help)**               Displays this help message.
+                 **($channel add|remove)** Adds or removes the channel to/from the whitelist.
+                 **($role add|remove)**    Adds or removes the minimum role required to use the bot on a server.
+                 **($csv)**                Get a specific CSV file from a list of all events.
+                 **($event)**              Opens up a list of events and their descriptions.` 
 
 const helpMessage = `
 \`\`\`
@@ -24,10 +46,6 @@ List of current commands:
     ($channel add|remove) Adds or removes the channel to/from the whitelist.
     ($role add|remove)    Adds or removes the minimum role required to use the bot on a server
     ($csv)                Get a specific CSV file from a list of all events.
-    Events:
-    ($event delete)       Delete event.
-    ($event PS2OP)        Sets up a PS2 Op type event.
-    ($event Training)     Sets up a Training type event.
-    ($event OW)           Sets up an OW type event.
+    ($event)              
 \`\`\`
 `
