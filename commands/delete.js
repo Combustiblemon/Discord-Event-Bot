199b5561e176e6b3979602da673b172586e0abde
Combustiblemon <tasos.hitiris@hotmail.com>
2021-02-12 21:33:51 +0200

    delete now uses the database

--- commands/delete.js
+++ commands/delete.js
@@ -3,6 +3,7 @@ const FileSystem = require('../src/services/FileSystem');
 const nullEmbedID = require('../src/models/error');
 const EventDetailsService = require('../src/services/EventDetailsService');
 const fs = require('fs');
+const SQLiteUtilities = require('../src/utils/SQLiteUtilities');
 //const EventScheduler = require('../src/services/EventScheduler');
 
 const messageTimeout = 600_000;
@@ -15,96 +16,120 @@ module.exports = {
      * @param {Discord.Message} message  
      */
     async execute(bot, message){
-        let originalChannel = message.channel;
 
+        //Get all the embeds currently tracked for the server
+        let embedArray = await FileSystem.getEmbedDataFromServerID(message.guild.id); //288682750792171520
 
-        //Get the embed names that exist in file
-        let tempArray = FileSystem.getEmbedNames(message.guild.name);
-        
-        //find only the messages in the current channel
-        tempArray = await this.messagesInChannel(message, tempArray);
-
-        if(!Array.isArray(tempArray) || !tempArray.length){
-            message.author.send('No events to delete.');
+        if(!embedArray){
+            message.author.send('\`\`\`No events to delete.\`\`\`');
             return; 
+        }
+
+        //find only the messages in the current channel
+        let tempArray = [];
+        embedArray.forEach(element => {
+            if(element.channelID === message.channel.id){
+                tempArray.push(element);
+            }
+        });
+
+        if(tempArray.length < 1){
+            message.author.send('\`\`\`No events to delete.\`\`\`');
+            return;
         }
         
         
         //Replace the '_' in the name with ' '
-        tempArray.forEach(function(item, index) {
-            tempArray[index] = item.replace(/_/gi, " ");
-        });
-        
-        const question = '```Which of the following events would you like to delete?\nCopy and paste the name below.\n     ' + tempArray.join("\n     ") + '```';
+        let events = [];
+        let eventList = '';
+        let text;
+        /* tempArray.forEach((element, index) => {
+            text = `${element.date.toISOString().substring(0, 10)} ${element.name}`;
+            events.push(text, element.embedID);
+            eventList += `${text}\n     `;
+        });*/
+
+        tempArray.sort(sortFunction);
+
+        for (let i = 0; i < tempArray.length; i++) {
+            text = `${tempArray[i].date.toISOString().substring(0, 10)} ${tempArray[i].name}`;
+            events.push([text, tempArray[i].embedID]);
+            eventList += `${text}\n     `;
+        }
+        eventList = eventList.substring(0, eventList.length - 6);
+        const question = '```Which of the following events would you like to delete?\nCopy and paste the name below.\n     ' + eventList + '```';
 
         let answer = await EventDetailsService.prototype.requestSingleDetail(question, message);
         if (!answer) return;
         
         //replace the ' ' in the answer with '_' so it matches the file naming structure 
-        answer = answer.replace(/ /gi, '_');
+        //answer = answer.replace(/ /gi, '_');
         //check if the the name exists
-        if(FileSystem.embedNameExists(answer, message.guild.name)){
-            this.deleteEmbed(answer.replace(/ /gi, '_'), message);
-            message.author.send(`Event \`${answer}\` deleted.`);
+        let eventIndex = findEventIndex(events, answer);
+        if(eventIndex){
+            this.deleteEmbed(events[eventIndex][1], message);
+            console.log(new Date(), `User "${message.author.username}" deleted event "${events[eventIndex][0]}" in server "${message.guild.name}"`);
+            message.author.send(`\`\`\`Event \`${answer}\` deleted.\`\`\``);
         }else{
-            message.author.send('Invalid name.');
+            message.author.send(`\`\`\`"${answer}" not found.\`\`\``);
         }
     },
 
     /**
      * 
-     * @param {string} answer
+     * @param {string} msgID
      * @param {Discord.Message} message 
      */
-    deleteEmbed(answer, message){
-        //convert the embed name into ID
-        let msgID = FileSystem.getEmbedID(answer, message.guild.id);
-        if (!msgID){
-            console.error(new nullEmbedID('Embed does not exist in memory'));
-            return;
-        }
-
+    deleteEmbed(msgID, message){
         //delete the message
-        message.channel.messages.fetch(msgID).then(msg =>{ msg.delete()}).catch(error => {console.error(error)});
+        message.channel.messages.fetch(msgID).then(msg =>{ msg.delete()}).catch(error => {console.error(new Date(), error)});
+        //delete the event in database
+        SQLiteUtilities.deleteData('EVENTS', {query: 'embed_id = ?', values: [msgID]});
         
         //remove references to embed
-        FileSystem.removeEmbedID(FileSystem.getEmbedID(answer, message.guild.id));
-        FileSystem.removeEmbedName(answer, message.guild.name);
+        //FileSystem.removeEmbedName(answer, message.guild.name);
         //EventScheduler.removeEventFromCheck(answer);
-
-        this.removeFiles(answer, message.guild.id);
     },
 
-    /**
-     * 
-     * @param {Discord.Message} message 
-     * @param {Array} embedNames 
-     * @returns {Array}
-     */
-    messagesInChannel(message, embedNames){
-        let temp = [];
-        embedNames.forEach(function (item, index) {
-            //if the channel of the embed is different from the channel delete was used in it removes it.
-            if (FileSystem.getEmbedChannel(item, message.guild.name) === message.channel.id) {
-                temp.push(item); 
-            }
-        })
-        
-        return Array.from(temp);
-    },
+    
+    
 
     removeFiles(filename, server){
 
         //remove the files
         fs.unlink(`./embeds/${server}/` + filename + '.json', (err) => {
             if (err) throw err;
-            console.log(`embeds/${server}/${filename}.json was deleted.`);
+            console.log(new Date(), `embeds/${server}/${filename}.json was deleted.`);
         });
 
         fs.unlink(`./events/${server}/` + filename + '.json', (err) => {
             if (err) throw err;
-            console.log(`events/${server}/${filename}.json was deleted.`);
+            console.log(new Date(), `events/${server}/${filename}.json was deleted.`);
         });
     }
 }
 
+/**
+ * 
+ * @param {Array} events The event array
+ * @param {String} eventName the formated event name
+ */
+function findEventIndex(events, eventName){
+    
+    for (let index = 0; index < events.length; index++) {
+        if (events[index][0] === eventName) return index;
+    }
+
+    return null;
+}
+
+
+function sortFunction(a, b) {
+    if (a.date === b.date) {
+        return 0;
+    }
+    else {
+        if(a.date > b.date) return 1
+        if(a.date < b.date) return -1
+    }
+}
